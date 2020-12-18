4baabb6ea2ad25951ac7560a914e4988a09a49fa
Victor-Balan <75117309+Victor-Balan@users.noreply.github.com>
2020-12-18 17:49:57 -0600

    v1.5.3

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
-const history = require('connect-history-api-fallback');
 
 const app = express();
 
