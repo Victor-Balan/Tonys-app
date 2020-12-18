aa80da6f4983c4cf035b8752f6decbb1155f16c1
Victor-Balan <75117309+Victor-Balan@users.noreply.github.com>
2020-12-18 17:40:52 -0600

    v1.5.2

--- server.js
+++ server.js
@@ -5,21 +5,7 @@ const history = require('connect-history-api-fallback');
 
 const app = express();
 
-
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
-
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
-
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
 
 const port = process.env.PORT || 8080;
 app.listen(port);
